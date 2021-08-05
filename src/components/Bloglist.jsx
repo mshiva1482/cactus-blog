53426da2a8939fe37fbd575fafa22c7a791a9078
shiva <mshiva1482@gmail.com>
2021-08-05 08:11:01 +0530

    Day 3

--- src/components/Bloglist.jsx
+++ src/components/Bloglist.jsx
@@ -1,16 +1,24 @@
 //DE-STRUCTURED PROPS
-const Bloglist = ({ blogs }) => {
+const Bloglist = ({ blogs, title, handleDelete }) => {
     // const blogs = props.blogs;
 
-    //KEY PROPERTY - used by react to keep track of each item in the DOM - must be unique
+    //Here, handleDelete is a function that is passeed in as a prop for deleting a blog based on its id.
+    //HandleDelete is defined within Home.jsx because the blogs array will be updated using setBlog (useState) so that it can be watched by react rather than deleting it manually.
+
+
     return (
         <div className="blog-list">
+            <h1>{title}</h1>
             { blogs.map((blog) => (
                 <div className="blog-preview" key = {blog.id}>
+                    {/* KEY PROPERTY - used by react to keep track of each item in the DOM - must be unique. */}
                    <h2>{blog.title}</h2> 
                    <p>Written by {blog.author}</p>
+                    
+                    {/*Function is put inside of an anonymous function because we are passing in blog.id as a parameter to the function.*/}
+                   <button onClick={() => {handleDelete(blog.id)}}>Delete blog</button>      
                 </div>
-            )) }            
+            )) }      
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Bloglist from "./Bloglist";
+
+//useState hook re-renders the DOM everytime a change in state is observed.
+//useEffect takes a function as a parameter and does not return any data - It invokes the function everytime the DOM is rendered.
+//Never use set function (of useState) within a useEffect function as it would  lead  to an infite loop of DOM re-renders.
 
 const Home = () => {
     //INTRO TO USESTATE HOOK
@@ -9,17 +13,34 @@ const Home = () => {
     //     setName("Sandeep");
     //     setAge(19);
     // }   
+    
+    const [blogs, setBlogs] = useState(null);
 
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'Lorem ipsum...', author: 'mario', id: 1},
-        {title: 'My new App', body: 'Lorem ipsum...', author: 'yoshi', id: 2},
-        {title: 'Welcome party', body: 'Lorem ipsum...', author: 'luigi', id: 3}
-    ]);
+    const [name, setName] = useState('mario');
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    //useEffect can have state variables as dependencies (put into one sigle array) - this would mean the function will be triggered once in the beginning and everytime the state of the mentioned variable changes. 
+    //Currently this effect runs only once in the beginning when the DOM is rendered.
+    useEffect( () => {
+        fetch('http://localhost:8000/blogs').then(res => {
+            return res.json();
+        }).then(data => {
+            setBlogs(data);
+        });
+    }, []);
     //PROPS
+    //EXAMPLE for reusable components in React
     return (
         <div className = "home">
-            <Bloglist blogs={blogs} />
+            {blogs && <Bloglist blogs={blogs} title = "All blogs" handleDelete = {handleDelete}/>}
+            {/* <Bloglist blogs={blogs.filter((blog) => blog.author === 'mario')} title = "Mario's blogs"/> */}
+
+            <button onClick={() => setName('luigi')}>Change name</button>
+            <h3>{ name }</h3>
         </div>
     )
 }
